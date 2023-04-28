f04c438b90b80bc5fee127636ffbdb62c9f8b25d
zZubat <leonardomagerote@gmail.com>
2023-04-28 20:43:17 -0300

    tela principal

---
+++ burguernight/src/componentes/TelaPrincipal/index.tsx
@@ -0,0 +1,9 @@
+import Tela from '../Tela'
+
+function TelaPrincipal(){
+    return(
+        <Tela>
+            <button><img src="../../Aseets/icons8-shopping-cart-24.png" alt="" /></button>
+        </Tela>
+    );
+}
