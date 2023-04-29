2f57a3dc5d5f48245d30f09073bbdf8e01bfe92b
pedro <phlaurentino2003@gmail.com>
2023-04-28 21:16:48 -0300

    Adiciona roteamento de telas

--- burguernight/src/App.tsx
+++ burguernight/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import TelaPedido from './componenetes/TelaPedidos';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/pedidos" element={<TelaPedido />} />
+      </Routes>
     </div>
   );
 }
---
+++ burguernight/src/componenetes/TelaPedidos.tsx
@@ -0,0 +1,12 @@
+import Tela from "./tela";
+
+function TelaPedido() {
+    return (
+        <Tela titulo="Resumo do seu pedido">
+            <div>Resumo</div>
+            <p>finalizar...</p>
+        </Tela>
+    );
+}
+
+export default TelaPedido;
