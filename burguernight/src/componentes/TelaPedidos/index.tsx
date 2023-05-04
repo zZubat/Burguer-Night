3dc674ab3a79f60a98aae65411adf4d40aa1f593
zZubat <leonardomagerote@gmail.com>
2023-05-04 19:12:54 -0300

    tela prduto

---
+++ burguernight/src/componentes/TelaPedidos/index.tsx
@@ -0,0 +1,12 @@
+import Tela from "../Tela";
+
+function TelaPedido() {
+    return (
+        <Tela>
+            <div>Resumo</div>
+            <p>finalizar...</p>
+        </Tela>
+    );
+}
+
+export default TelaPedido;
