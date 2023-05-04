0ceacd6b771446ab2b1b38488b519b9d61b72c65
zZubat <leonardomagerote@gmail.com>
2023-05-03 21:43:49 -0300

    att app

--- burguernight/src/App.tsx
+++ burguernight/src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-import TelaPedido from './componentes/TelaPedidos';
+import TelaPedido from './componentes/TelaPedidos/index';
 import { Route, Routes } from 'react-router-dom';
-import TelaPrincipal from './componentes/TelaPrincipal';
+import TelaPrincipal from './componentes/TelaPrincipal/index';
+
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/pedidos" element={<TelaPedido />} />
-        <Route path="/" element={<TelaPrincipal/>} />
+        <Route path="/" element={<TelaPrincipal/>} /> 
       </Routes>
     </div>
   );
