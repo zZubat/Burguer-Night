d1e6972974845361821aaf6e4911670338412c44
1natan <n13santiago@gmail.com>
2023-05-12 22:17:47 -0300

    Restauração no Código da Tela Pedidos.

---
+++ burguernight/src/componentes/Adicionais/Caixa.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+import styles from './Caixa.module.css'
+
+type Props = {
+    titulo: string;
+    children: ReactNode;
+};
+
+function Caixa (props: Props) {
+    
+    return (
+            <div className={styles.borda}>
+                <h3 className={styles.titulo}>{props.titulo}</h3>
+                <div className={styles.conteudo}>
+                    {props.children}
+                </div>
+            </div>
+    );
+}
+
+export default Caixa;
--- burguernight/src/componentes/Tela/index.tsx
+++ burguernight/src/componentes/Tela/index.tsx
@@ -1,18 +1,19 @@
 import { ReactNode } from "react"
 import styles from './index.module.css'
-import imagem from '../../Aseets/backgroundConfirmarPedido.png'
 
 type Props = {
     children: ReactNode;
-    //titulo: string;
+    barraTopo?: ReactNode;
 }
 
 function Tela(props:Props){
     return(
-        <div>
-            <img className={styles.imagem} src={imagem}/>
-            <div className={styles.tela}>
-            {props.children}
+        <div className={styles.tela}>
+            <div>
+                {props.barraTopo}
+            </div>
+            <div className={styles.conteudo}>
+                {props.children}
             </div>
         </div>
     );
