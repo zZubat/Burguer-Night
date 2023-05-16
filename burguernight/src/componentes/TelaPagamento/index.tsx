aca993d4d3964a5a7e17085bc724df9c3a5353cf
1natan <n13santiago@gmail.com>
2023-05-15 22:28:28 -0300

    Pagamento

--- burguernight/src/componentes/TelaPagamento/index.tsx
+++ burguernight/src/componentes/TelaPagamento/index.tsx
@@ -1,8 +1,20 @@
+import { CaretLeft, Link } from '@phosphor-icons/react';
+import Tela from '../Tela'
 import styles from './index.module.css'
 
 function Pagamento() {
     return (
-        <div></div>
+        <Tela barraTopo={
+            <Link to="/">
+                <a className={styles.iconback}>
+                    <CaretLeft color="#fff" size={42} />
+                 </a>
+            </Link>
+        }>
+        <div>
+            
+        </div>       
+        </Tela>
     )
 }
 
