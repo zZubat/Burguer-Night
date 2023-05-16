e6b721765a48c003762a727d62d50446eb7f2024
1natan <n13santiago@gmail.com>
2023-05-15 22:28:39 -0300

    Pagamento

--- burguernight/src/componentes/TelaPagamento/index.tsx
+++ burguernight/src/componentes/TelaPagamento/index.tsx
@@ -1,4 +1,5 @@
-import { CaretLeft, Link } from '@phosphor-icons/react';
+import { CaretLeft } from '@phosphor-icons/react';
+import { Link } from 'react-router-dom';
 import Tela from '../Tela'
 import styles from './index.module.css'
 
@@ -7,12 +8,17 @@ function Pagamento() {
         <Tela barraTopo={
             <Link to="/">
                 <a className={styles.iconback}>
-                    <CaretLeft color="#fff" size={42} />
+                    <div>
+                        <CaretLeft color="#000" size={42} />
+                    </div>
+                    <div>
+                        <h3>Finalizar Pedido</h3>
+                    </div>  
                  </a>
             </Link>
         }>
         <div>
-            
+            <h3>Teste</h3>
         </div>       
         </Tela>
     )
