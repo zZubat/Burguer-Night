29a8dc75bec2385ab9024c88344428f294c7aa18
zZubat <leonardomagerote@gmail.com>
2023-05-23 21:40:37 -0300

    navegação

--- burguernight/src/componentes/TelaPrincipal/Navegação/index.tsx
+++ burguernight/src/componentes/TelaPrincipal/Navegação/index.tsx
@@ -1,9 +1,10 @@
+import Caixa from '../../Adicionais/Caixa';
 import styles from './index.module.css'
 
 function Navegacao(){
     return(
         <span>
-            <nav className={styles.Navegacao}>
+            <nav className={styles.navegacao}>
                 <a href="#Burguer" >Burguer</a>
                 <a href="#Bebidas" >Bebidas</a>
                 <a href="#Porções" >Porções</a>
