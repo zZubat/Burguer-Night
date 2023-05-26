891903138ac249de0fd9c739b42905cba10936d1
negaopedrozk <phlaurentino2003@gmail.com>
2023-05-25 22:16:56 -0300

    alterar pagamento, cpf

--- burguernight/src/componentes/Adicionais/BoxFormasdePagamento/BoxFormasdePagamento.tsx
+++ burguernight/src/componentes/Adicionais/BoxFormasdePagamento/BoxFormasdePagamento.tsx
@@ -6,13 +6,21 @@ import imgCpf from "./imagens/LOGO DOCUMENTO.png"
 
 
 function BoxFormasdePagamento () {
-    const [aberto, setAberto] = useState(false);
+    const [alterarpagamento, setPagamento] = useState(false);
+    const [alterardocumento, setDocumento] = useState(false);
+
 
     function abrir() {
-        setAberto(true);
+        setPagamento(true);
     }
     function fechar() {
-        setAberto(false);
+        setPagamento(false);
+    }
+    function open() {
+        setDocumento(true);
+    }
+    function close() {
+        setDocumento(false);
     }
 
 
@@ -20,39 +28,70 @@ function BoxFormasdePagamento () {
         <div className={styles.borda}>
             <div className={styles.BoxFormasdePagamento}>
                 <div className={styles.logo}>
-                    <img src = {imgElo}/>
-                    <img src = {imgPix}/>
+                        <img src = {imgElo}/>
+                        <img src = {imgPix}/>
                 </div>
-                <div className= {styles.f}>
-                    <h1>Formas de pagamento</h1>
-                    <h3>Dinheiro - Cartão - Pix</h3>
-                </div>
-                <button className={styles.botao}>Alterar</button>
-            </div>
-                <div className={styles.BoxFormasdePagamento}>
-                    <img className={styles.cpf}  src = {imgCpf}/>
-                    <div className={styles.BoxDocumento}>
-                    <header className={styles.row}>
-                        <div className={styles.botaocpf}>
-                            <h1 >CPF/CNPJ na nota</h1>
-                            <h3 >Digite um documento válido</h3>
-                        </div>
-                            {(aberto) ? (
-                    <button className={styles.botao} onClick={fechar}>Fechar</button>
-                    ) : (
-                    <button className={styles.botao} onClick={abrir}>Alterar</button>
-                )}
-                    
-                    </header>
+                <header className= {styles.pagamento}>
+                    <div className= {styles.escolhapagamento}>
+                        <h1>Formas de pagamento</h1>
+                        <h3>Dinheiro - Cartão - Pix</h3>
                     </div>
+                    {(alterarpagamento) ? (
+                        <button className={styles.botao} onClick={fechar}>Fechar</button>
+                        ) : (
+                        <button className={styles.botao} onClick={abrir}>Alterar</button>
+                    )}
+                </header>
+            </div>
+
+            {(alterarpagamento) && (
+                <div className={styles.boxPagar}>
+                    <h1>Formas de pagamento</h1>
+                    
+                    <div  className={styles.comopagar}>
+                        <h3>Cartão de crédito</h3>                   <button className={styles.botao}>Selecionar</button>
+                    </div>
+                    
+                    <div className={styles.comopagar}>
+                        <hgroup>
+                        <h3>Pix</h3>
+                        </hgroup>
+                    <button className={styles.botao}>Selecionar</button>
+                    </div>
+                    
+                    <div className={styles.comopagar}>
+                        <hgroup>
+                            <h3>Dinheiro</h3>
+                        </hgroup>
+                    <button className={styles.botao}>Selecionar</button>
+                    </div>
+                    
                 </div>
                 
+            )}
+           
+            <div className={styles.BoxFormasdePagamento}>
+                <img className={styles.cpf}  src = {imgCpf}/>
+                <div className={styles.BoxDocumento}>
+                    <header className={styles.documento}>
+                        <div className={styles.boxAddcpf}>
+                            <h1 >CPF/CNPJ na nota</h1>
+                            <textarea className={styles.textarea}>Digite um documento válido</textarea>
+                        </div>
+                                    {(alterardocumento) ? (
+                            <button className={styles.botao} onClick={close}>Fechar</button>
+                            ) : (
+                            <button className={styles.botao} onClick={open}>Alterar</button>
+                        )}
+                    </header>
+                </div>
+            </div>
                 
-                {(aberto) && (
+                {(alterardocumento) && (
                 <div className={styles.boxCPF}>
                     <hgroup>
                         <h1>CPF/CNPJ</h1>
-                        <h3>Insira um CPF/CNPJ válido</h3>
+                        <textarea className={styles.textarea}>Insira um CPF/CNPJ válido</textarea>
                     </hgroup>
                     <button className={styles.botao}>Confirmar</button>
                 </div>
